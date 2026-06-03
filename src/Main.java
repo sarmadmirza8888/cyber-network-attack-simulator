b08a82fd379a6b58d411cb9cb4e2158021ee9df6
sarmadmirza8888 <sarmadmirza8888@gmail.com>
2026-06-03 19:40:54 +0500

    updating main (integration done with todays classes)

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,93 @@
 public class Main {
     public static void main(String[] args) {
-        // =========================
-        // CREATE NETWORK MANAGER
-        // =========================
-        NetworkManager manager = new NetworkManager();
-        // =========================
-        // CREATE DEVICES
-        // =========================
+        // Managers
+        NetworkManager networkManager = new NetworkManager();
+        QueueManager queueManager = new QueueManager();
+        Firewall firewall = new Firewall();
+        RollBackManager RollBackManager = new RollBackManager();
+        // Devices
         Device pc1 = new Device(
                 "PC1",
                 "192.168.1.1",
                 "PC"
         );
-        Device router1 = new Device(
-                "Router1",
-                "192.168.1.2",
-                "ROUTER"
-        );
         Device server1 = new Device(
                 "Server1",
-                "192.168.1.10",
+                "192.168.1.2",
                 "SERVER"
         );
-        // =========================
-        // ADD DEVICES
-        // =========================
-
-        manager.addDevice(pc1);
-        manager.addDevice(router1);
-        manager.addDevice(server1);
-
-        // =========================
-        // CREATE CONNECTIONS
-        // =========================
-
-        Connection c1 = new Connection(pc1, router1, 5);
-
-        Connection c2 = new Connection(router1, server1, 10);
-
-        // =========================
-        // ADD CONNECTIONS
-        // =========================
-
-        manager.addConnection(c1);
-        manager.addConnection(c2);
-
-        // =========================
-        // CREATE PACKET
-        // =========================
-
-        Packet packet1 = new Packet(pc1, server1, "Hello Server", false);
-
-        // =========================
-        // SEND PACKET
-        // =========================
-        manager.sendPacket(packet1);
-        // =========================
-        // SHOW DEVICES
-        // =========================
-        manager.showAllDevices();
-        // =========================
-        // SHOW CONNECTIONS
-        // =========================
-        manager.showConnections();
-        // =========================
-        // SHOW NETWORK
-        // =========================
-        NetworkVisualizer.displayNetwork(manager.getConnections()
+        Device router1 = new Device(
+                "Router1",
+                "192.168.1.3",
+                "ROUTER"
         );
-        // =========================
-        // SHOW LOGS
-        // =========================
-        manager.showLogs();
+        // Add Devices
+        networkManager.addDevice(pc1);
+        networkManager.addDevice(server1);
+        networkManager.addDevice(router1);
+        // Connections
+        networkManager.addConnection(
+                new Connection(pc1, router1, 5)
+        );
+        networkManager.addConnection(
+                new Connection(router1, server1, 3)
+        );
+        // Save Network State
+        NetworkState state =
+                new NetworkState(
+                        networkManager.getDevices()
+                );
+
+        RollBackManager.saveState(state);
+        // Normal Packet
+        Packet normalPacket =
+                new Packet(
+                        pc1,
+                        server1,
+                        "Hello Server",
+                        false
+                );
+        // Malware Packet
+        Packet malwarePacket =
+                new Packet(
+                        pc1,
+                        server1,
+                        "Virus.exe",
+                        true
+                );
+        // Add To Queue
+        queueManager.enqueuePacket(normalPacket);
+        queueManager.enqueuePacket(malwarePacket);
+        // Process Queue
+        while (!queueManager.isEmpty()) {
+            Packet currentPacket =
+                    queueManager.dequeuePacket();
+            if (firewall.inspectPacket(currentPacket)) {
+                networkManager.sendPacket(
+                        currentPacket
+                );
+                // Malware Simulation
+                if (currentPacket.isInfected()) {
+                    currentPacket
+                            .getDestination()
+                            .infect();
+                    System.out.println(
+                            "\n"
+                                    + currentPacket
+                                    .getDestination()
+                                    .getDeviceName()
+                                    + " has been infected!"
+                    );
+                }
+            }
+        }
+        // Display Devices
+        networkManager.showAllDevices();
+        // Display Connections
+        networkManager.showConnections();
+        // Display Logs
+        networkManager.showLogs();
+        // Rollback Demo
+        RollBackManager.rollback();
     }
 }
