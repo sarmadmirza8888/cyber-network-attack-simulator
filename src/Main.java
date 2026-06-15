b181e510f0d5e13a52b3307b7eccb69dd660b88d
sarmadmirza8888 <sarmadmirza8888@gmail.com>
2026-06-15 23:49:26 +0500

    checking main

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,89 @@
 public class Main {
-    public static void main(String[] args) {
-        // Managers
-        NetworkManager networkManager = new NetworkManager();
-        QueueManager queueManager = new QueueManager();
-        Firewall firewall = new Firewall();
-        RollBackManager RollBackManager = new RollBackManager();
-        // Devices
-        Device pc1 = new Device(
-                "PC1",
-                "192.168.1.1",
-                "PC"
-        );
-        Device server1 = new Device(
-                "Server1",
-                "192.168.1.2",
-                "SERVER"
-        );
-        Device router1 = new Device(
-                "Router1",
-                "192.168.1.3",
-                "ROUTER"
-        );
-        // Add Devices
-        networkManager.addDevice(pc1);
-        networkManager.addDevice(server1);
-        networkManager.addDevice(router1);
-        // Connections
-        networkManager.addConnection(
-                new Connection(pc1, router1, 5)
-        );
-        networkManager.addConnection(
-                new Connection(router1, server1, 3)
-        );
-        // Save Network State
-        NetworkState state =
-                new NetworkState(
-                        networkManager.getDevices()
-                );
 
-        RollBackManager.saveState(state);
-        // Normal Packet
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("CYBER ATTACK NETWORK SIMULATOR");
+        System.out.println("=================================");
+
+        // =====================================================
+        // MANAGERS
+        // =====================================================
+
+        NetworkManager networkManager =
+                new NetworkManager();
+
+        QueueManager queueManager =
+                new QueueManager();
+
+        Firewall firewall =
+                new Firewall();
+
+        RollBackManager rollbackManager =
+                new RollBackManager();
+        Graph graph = new Graph();
+        ThreatTree threatTree = new ThreatTree();
+        MalwareSimulator malwareSimulator = new MalwareSimulator();
+        // =====================================================
+        // DEVICES
+        // =====================================================
+        Device pc1 = new Device("PC1", "192.168.1.10", "Computer");
+        Device router1 = new Device("Router1", "192.168.1.1", "Router");
+        Device server1 = new Device("Server1", "192.168.1.100", "Server");
+        Device firewallDevice = new Device("Firewall1", "192.168.1.254", "Firewall");
+        // =====================================================
+        // ADD DEVICES
+        // =====================================================
+        networkManager.addDevice(pc1);
+        networkManager.addDevice(router1);
+        networkManager.addDevice(server1);
+        networkManager.addDevice(firewallDevice);
+        networkManager.showAllDevices();
+        // =====================================================
+        // CONNECTIONS
+        // =====================================================
+        Connection c1 = new Connection(pc1, router1, 5);
+        Connection c2 = new Connection(router1, server1, 3);
+        Connection c3 = new Connection(server1, firewallDevice, 2);
+        networkManager.addConnection(c1);
+        networkManager.addConnection(c2);
+        networkManager.addConnection(c3);
+        networkManager.showConnections();
+        // =====================================================
+        // SAVE NETWORK STATE
+        // =====================================================
+        NetworkState state = new NetworkState(networkManager.getDevices()
+                );
+        rollbackManager.saveState(state);
+        // =====================================================
+        // GRAPH
+        // =====================================================
+        graph.addDevice(pc1);
+        graph.addDevice(router1);
+        graph.addDevice(server1);
+        graph.addDevice(firewallDevice);
+        graph.addConnection(pc1, router1);
+        graph.addConnection(router1, server1);
+        graph.addConnection(server1, firewallDevice);
+        graph.displayGraph();
+        // =====================================================
+        // THREATS (BST)
+        // =====================================================
+        Threat t1 = new Threat("Trojan", 8, "Steals user data");
+        Threat t2 = new Threat("Spyware", 4, "Monitors activity");
+        Threat t3 = new Threat("Ransomware", 10, "Encrypts files");
+        Threat t4 = new Threat("Worm", 6, "Self-spreading malware");
+        threatTree.insertThreat(t1);
+        threatTree.insertThreat(t2);
+        threatTree.insertThreat(t3);
+        threatTree.insertThreat(t4);
+
+        threatTree.displayThreats();
+
+        // =====================================================
+        // PACKETS
+        // =====================================================
+
         Packet normalPacket =
                 new Packet(
                         pc1,
@@ -47,7 +91,7 @@ public class Main {
                         "Hello Server",
                         false
                 );
-        // Malware Packet
+
         Packet malwarePacket =
                 new Packet(
                         pc1,
@@ -55,39 +99,71 @@ public class Main {
                         "Virus.exe",
                         true
                 );
-        // Add To Queue
+
         queueManager.enqueuePacket(normalPacket);
         queueManager.enqueuePacket(malwarePacket);
-        // Process Queue
+
+        queueManager.displayQueue();
+
+        // =====================================================
+        // PROCESS QUEUE
+        // =====================================================
+
         while (!queueManager.isEmpty()) {
+
             Packet currentPacket =
                     queueManager.dequeuePacket();
+
             if (firewall.inspectPacket(currentPacket)) {
+
                 networkManager.sendPacket(
                         currentPacket
                 );
-                // Malware Simulation
-                if (currentPacket.isInfected()) {
-                    currentPacket
-                            .getDestination()
-                            .infect();
-                    System.out.println(
-                            "\n"
-                                    + currentPacket
-                                    .getDestination()
-                                    .getDeviceName()
-                                    + " has been infected!"
-                    );
-                }
             }
         }
-        // Display Devices
-        networkManager.showAllDevices();
-        // Display Connections
-        networkManager.showConnections();
-        // Display Logs
+        // =====================================================
+        // BFS
+        // =====================================================
+        Bfs bfs = new Bfs();
+        bfs.traverse(
+                graph,
+                pc1
+        );
+        // =====================================================
+        // DFS
+        // =====================================================
+        Dfs dfs = new Dfs();
+        dfs.traverse(
+                graph,
+                pc1
+        );
+        // =====================================================
+        // DIJKSTRA
+        // =====================================================
+        Dijkstra dijkstra = new Dijkstra();
+        dijkstra.shortestPath(graph, pc1
+        );
+        // =====================================================
+        // MALWARE SIMULATION
+        // =====================================================
+        malwareSimulator.spreadMalware(graph, pc1);
+        // =====================================================
+        // LOGS
+        // =====================================================
         networkManager.showLogs();
-        // Rollback Demo
-        RollBackManager.rollback();
+        // =====================================================
+        // ROLLBACK
+        // =====================================================
+        rollbackManager.displaySavedStates();
+        rollbackManager.rollback();
+        rollbackManager.displaySavedStates();
+        // =====================================================
+        // VISUALIZER
+        // =====================================================
+        NetworkVisualizer visualizer = new NetworkVisualizer();
+        visualizer.displayNetwork(networkManager);
+        System.out.println(
+                "\nSimulation Completed Successfully."
+        );
     }
 }
--- src/NetworkVisualizer.java
+++ src/NetworkVisualizer.java
@@ -1,10 +1,7 @@
 import java.util.*;
 public class NetworkVisualizer {
-    public static void displayNetwork(
-            LinkedList<Connection> connections) {
-        System.out.println(
-                "\n===== NETWORK TOPOLOGY ====="
-        );
+    public static void displayNetwork(LinkedList<Connection> connections) {
+        System.out.println("\n===== NETWORK TOPOLOGY =====");
         if (connections.isEmpty()) {
             System.out.println(
                     "No connections available."
@@ -13,7 +10,27 @@ public class NetworkVisualizer {
         }
         for (Connection connection : connections) {
             System.out.println(
-                    "[" +connection.getSource().getDeviceName() + "] -----> [" + connection.getDestination().getDeviceName() + "]"
+                    "[" + connection.getSource().getDeviceName() + "] -----> [" + connection.getDestination().getDeviceName() + "]"
+            );
+        }
+    }
+
+    public void displayNetwork(
+            NetworkManager networkManager) {
+
+        System.out.println(
+                "\n===== NETWORK VISUALIZATION ====="
+        );
+
+        for (Connection connection :
+                networkManager.getConnections()) {
+
+            System.out.println(
+                    connection.getSource()
+                            .getDeviceName()
+                            + " ---- "
+                            + connection.getDestination()
+                            .getDeviceName()
             );
         }
     }
